25ad216d75ace3cd99aa618b40b1a8665f135317
nico-Zero <zandaxheart955@gmail.com>
2024-08-04 09:43:33 +0530

    miner change in cht.sh in LSD.
    M  exit.sh
     M ../../setup.sh

--- .config/LSD/exit.sh
+++ .config/LSD/exit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-        clear
 if [ ! -z $TMUX ] && [ -z $NVIM ]; then
     # # The commanted code for when you want to exit detach if only 1 window is left open.
     # if [ 1 -eq "$(tmux list-windows | wc -l)" ]; then
@@ -10,6 +9,7 @@ if [ ! -z $TMUX ] && [ -z $NVIM ]; then
 
 
     if [ 1 -eq "$(tmux display-message -p "#I")" ] && [ 1 -eq "$(tmux display-message -p "#{pane_index}")" ]; then
+        clear
         tmux detach
         exit 1
     fi
