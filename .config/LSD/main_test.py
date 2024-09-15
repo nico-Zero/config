f5b392d55705e2731fa852535f394315832e0541
nico-Zero <zandaxheart955@gmail.com>
2024-09-15 18:12:02 +0530

    update.
    M  .config/LSD/encode.png
    M  .config/LSD/image_encode_decode
    M  .config/LSD/main_test.py
    A  .config/LSD/n.txt

--- .config/LSD/main_test.py
+++ .config/LSD/main_test.py
@@ -1,28 +1,28 @@
-from itertools import cycle
-from PIL import Image
-
-
-def decode(img_path, delimiter: str = "11111110"):
-    image = Image.open(img_path)
-    image = image.convert("RGB")
-    pixels = list(image.getdata())  # type: ignore
-    binary_message = ""
-
-    iter = cycle(range(len(pixels[0])))
-    for pixel in pixels:
-        cp = next(iter)
-        channel = pixel[cp]
-        binary_message += str(channel & 1)
-
-    binary_chars = [binary_message[i : i + 8] for i in range(0, len(binary_message), 8)]
-    message = ""
-    for binary_char in binary_chars:
-        if binary_char == delimiter:
-            break
-        message += chr(int(binary_char, 2))
-        print(chr(int(binary_char, 2)))
-    return message
-
-
-d = decode("./encode.png")
-# print(d)
+# from itertools import cycle
+# from PIL import Image
+#
+#
+# def decode(img_path, delimiter: str = "11111110"):
+#     image = Image.open(img_path)
+#     image = image.convert("RGB")
+#     pixels = list(image.getdata())  # type: ignore
+#     binary_message = ""
+#
+#     iter = cycle(range(len(pixels[0])))
+#     for pixel in pixels:
+#         cp = next(iter)
+#         channel = pixel[cp]
+#         binary_message += str(channel & 1)
+#
+#     binary_chars = [binary_message[i : i + 8] for i in range(0, len(binary_message), 8)]
+#     message = ""
+#     for binary_char in binary_chars:
+#         if binary_char == delimiter:
+#             break
+#         message += chr(int(binary_char, 2))
+#         print(chr(int(binary_char, 2)))
+#     return message
+#
+#
+# d = decode("./encode.png")
+# # print(d)
