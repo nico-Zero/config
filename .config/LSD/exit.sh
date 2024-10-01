f25d7809dfc5e75c206d8adc95cd8d16cc8b2058
nico-Zero <zandaxheart955@gmail.com>
2024-10-01 18:43:58 +0530

    modified the exit code for asdf
    M  .config/LSD/exit.sh

--- .config/LSD/exit.sh
+++ .config/LSD/exit.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/bash
 if [ ! -z $TMUX ] && [ -z $NVIM ]; then
-    # # The commanted code for when you want to exit detach if only 1 window is left open.
-    # if [ 1 -eq "$(tmux list-windows | wc -l)" ]; then
-    #     clear
-    #     tmux detach
-    #     exit 1
-    # fi
-
-
-    if [ 1 -eq "$(tmux display-message -p "#I")" ] && [ 1 -eq "$(tmux display-message -p "#{pane_index}")" ]; then
+    if [ 2 -eq "$(tmux display-message -p '#I')" ]; then
+        tmux select-window -t 1
+        exit 1
+    elif [ 1 -eq "$(tmux display-message -p '#I')" ] && [ 1 -eq "$(tmux display-message -p '#{pane_index}')" ]; then
         clear
         tmux detach
         exit 1
